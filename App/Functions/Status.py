cce8d31c065fffd3b1e051b9c3d2ce2ebc0577ac
IAmGregorJ <iam@gregorj.org>
2022-05-05 21:55:43 +0200

    added more psutil info to "how are you feeling"

--- App/Functions/Status.py
+++ App/Functions/Status.py
@@ -1,7 +1,9 @@
 '''imports'''
 import os
-import psutil
+import time
 import Communication.Output as out
+import psutil
+
 
 class Status():
     '''answer to the question how are you?'''
@@ -9,14 +11,20 @@ class Status():
         self.cpu = self.get_cpu_usage_pct()
         self.temp = self.get_cpu_temp()
         self.ram = round(self.get_ram_usage() / 1024 / 1024 / 1024, 2)
+        self.uptime = self.get_uptime()
+        self.free = self.get_disk_free_percent()
+        self.battery = self.get_battery_status()
 
     def give_status(self):
         '''give an output of the current status'''
 
-        out.Output.say(f"I'm doing just fine thanks. My cpu load is at {self.cpu} percent"
-                    f" and my temperature is {self.temp} degrees."
-                    f" RAM usage is at {self.ram} gigabytes."
-                    f"The sun is shining somewhere in the world"
+        out.Output.say(f"I'm doing just fine thanks."
+                    f"It has been {self.sec_to_hours(self.uptime)} since boot."
+                    f"My cpu load is at {self.cpu} percent"
+                    f" and my temperature is {self.temp} degrees. "
+                    f"{self.free} percent of the main drive is free. "
+                    f" RAM usage is at {self.ram} gigabytes. "
+                    f"There is {self.sec_to_hours(self.battery)} battery time left"
                     " and you're here with me. I'm happy!")
     def get_cpu_usage_pct(self):
         '''the system's average CPU load as measured over a period of 500 ms'''
@@ -39,3 +47,23 @@ class Status():
     def get_ram_usage(self):
         '''Obtains the absolute number of RAM bytes currently in use by the system'''
         return int(psutil.virtual_memory().total - psutil.virtual_memory().available)
+    ## NEW FUNCTION
+    def get_uptime(self):
+        '''Gets the total time that the computer has been running since boot'''
+        return time.time() - psutil.boot_time()
+    def sec_to_hours(self, seconds):
+        '''convert seconds to hours'''
+        hours=str(round(seconds//3600))
+        minutes=str((round(seconds%3600)//60))
+        scnds=str((round(seconds%3600)%60))
+        d = f"{hours} hours, {minutes} minutes and {scnds} seconds"
+        return d
+    def get_disk_free_percent(self):
+        '''get percent of free disk space'''
+        disk = psutil.disk_usage('/')
+        percent_free = 100 - disk.percent
+        return percent_free
+    def get_battery_status(self):
+        '''Get battery power left'''
+        battery = psutil.sensors_battery()
+        return battery.secsleft
